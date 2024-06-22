6ca369b8d8fab94246bb3f3002df90de12f2ee4d
raihan-rahoof <mcrahoof28@gmail.com>
2024-06-22 09:46:17 +0530

    made change in admin private route

--- src/Context/AdminAuthContext.jsx
+++ src/Context/AdminAuthContext.jsx
@@ -1,16 +1,32 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 const AdminAuthContext = createContext();
 
 export const AdminAuthProvider = ({ children }) => {
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuthStatus = () => {
+      const access = JSON.parse(localStorage.getItem("admin_access"));
+      const refresh = JSON.parse(localStorage.getItem("admin_refresh"));
+
+      if (access && refresh) {
+        setIsAdminLoggedIn(true);
+      } else {
+        setIsAdminLoggedIn(false);
+      }
+      setLoading(false);
+    };
+
+    checkAuthStatus();
+  }, []);
 
   return (
-    <AdminAuthContext.Provider value={{ isAdminLoggedIn, setIsAdminLoggedIn }}>
+    <AdminAuthContext.Provider value={{ isAdminLoggedIn, setIsAdminLoggedIn, loading }}>
       {children}
     </AdminAuthContext.Provider>
   );
 };
-
 
 export default AdminAuthContext;
--- src/utlis/AdminPrivateRoute.jsx
+++ src/utlis/AdminPrivateRoute.jsx
@@ -3,7 +3,11 @@ import { Navigate } from 'react-router-dom';
 import AdminAuthContext from '../Context/AdminAuthContext';
 
 const AdminPrivateRoute = ({ element: Element, ...rest }) => {
-  const { isAdminLoggedIn } = useContext(AdminAuthContext);
+  const { isAdminLoggedIn , loading } = useContext(AdminAuthContext);
+
+  if (loading) {
+        return <div>Loading...</div>;
+  }
 
   return isAdminLoggedIn ? <Element {...rest} /> : <Navigate to="/admin" replace />;
 };
